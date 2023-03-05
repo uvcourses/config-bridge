d2ffc9cb0bfabd2880e3bda23cc2b0d3572e733c
uvcourses <uvcourses@gmail.com>
2023-03-05 00:29:15 -0600

    Your commit message

---
+++ src/main/java/com/learn/configbridge/ConfigBridgeApplication.java
@@ -0,0 +1,13 @@
+package com.learn.configbridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigBridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigBridgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learn/configbridge/ConfigBridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.configbridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigBridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
